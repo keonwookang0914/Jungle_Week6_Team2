5bd335c0ff4146443f9175f8b4aed527ca0a9443
HanYoungSeo <108516412+persona6whenrelease@users.noreply.github.com>
2026-04-14 00:49:51 +0900

    before pull

---
+++ NipsEngine/Source/Engine/Render/PostProcess/HeightFogPostProcess.cpp
@@ -0,0 +1,10 @@
+﻿#include "HeightFogPostProcess.h"
+
+bool UHeightFogPostProcess::IsEnabled(const FPostProcessViewDesc& ViewDesc) const
+{
+	return false;
+}
+
+void UHeightFogPostProcess::Execute(FD3DDevice* Device, ID3D11DeviceContext* Context, const FPostProcessViewDesc& ViewDesc, FRenderResources& Resources, const FRenderTargetSet& RenderTargets, ID3D11ShaderResourceView* SceneColorSRV, ID3D11RenderTargetView* OutputRTV)
+{
+}
